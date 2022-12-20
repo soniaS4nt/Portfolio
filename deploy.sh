64f9f62533841ac4198e413e8ed63b77ac2830da
“sonia” <soniachaparro21@gmail.com>
2022-12-20 15:00:14 -0300

    fix: deploy config

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:sonias4nt/Portfolio.git main:gh-pages
 
 cd -
+
