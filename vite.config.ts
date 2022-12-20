2f8b088116d033722c8164efaadafd010adc9068
“sonia” <soniachaparro21@gmail.com>
2022-12-20 15:50:49 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
   base: '/Portfolio/',
 })
+
+// I have tried to change the base to /Portfolio/ and /Portfolio/index.html but it doesn’t work.
+
+
 
 
 
