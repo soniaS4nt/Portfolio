8e83ae9f3b2e48b3816310d4eaf2fb235f725d8b
“sonia” <soniachaparro21@gmail.com>
2022-12-20 18:51:14 -0300

    fix: vite config

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import { Container, Image } from "@chakra-ui/react";
 import { Title } from "../../styles/styles";
+const programer = new URL("./programer1.svg", import.meta.url).href;
 
 const Home: React.FC = () => {
   return (
     <Container centerContent marginTop="20">
-      <Image width="20rem" src="/programer1.svg" alignItems="center" />
+      <Image width="20rem" src={programer} alignItems="center" />
       <Title>Sonia Chaparro</Title>
     </Container>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+/* export default defineConfig({
     base: '/Portfolio/',
-    assetsInclude: ['src/assets', 'src/assets/**/*'],
-    publicDir: 'src/assets',
+    
     /* build: {
     if (mode === 'production') {
       return {
@@ -18,9 +16,23 @@ export default defineConfig({
       }
       
     } */
+  
+
+  export default defineConfig(({ mode }) => {
+    if (mode === 'development') {
+      return {
+       base: '/',
+       plugins: [react()]
+      }
+    } else {
+      
+      return {
+        base: '/Portfolio/',
+        plugins: [react()]
+      }
+    }
+    
   })
-
-
 
 
 
