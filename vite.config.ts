9768c9fd7e006adc000da3a31be2e828ee4145e9
“sonia” <soniachaparro21@gmail.com>
2022-12-21 17:35:20 -0300

    fix: add script to  404.html & index.html & basename:/Portfolio to browserRouter

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 /* export default defineConfig({
-    base: '/Portfolio/',
-    
-    /* build: {
-    if (mode === 'production') {
-      return {
-        base: '/Portfolio/',
-      }
-    } else{
-      return {
-        base: '/',
-      }
-      
-    } */
+  plugins: [react()],
   
-
+}) */
   export default defineConfig(({ mode }) => {
     if (mode === 'development') {
       return {
