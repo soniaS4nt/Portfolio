d4d92bc4e86fea319f01015164996d435efac05e
“sonia” <soniachaparro21@gmail.com>
2022-12-25 23:09:26 -0300

    fix: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react  from '@vitejs/plugin-react'
 
 
 // https://vitejs.dev/config/
-/* export default defineConfig({
-  plugins: [react()],
-  
-}) */
   export default defineConfig({
-
       plugins: [react()],
       base: '/Portfolio/',
   })
