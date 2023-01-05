d01825ad612ec83f1654f67f002bf0f3b9aae74b
“sonia” <soniachaparro21@gmail.com>
2023-01-05 17:54:53 -0300

    fix href of  button

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -7,7 +7,6 @@ const NotFound: React.FC = () => {
         Lo siento no he encontrado lo que buscas, pero puedes volver a la página
         principal
       </Text>
-
       <Image width="15rem" margin={20} src="/nofound.svg" alt="404" />
     </Container>
   );
