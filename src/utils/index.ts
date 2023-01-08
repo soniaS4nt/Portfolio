ff2a2c1374e02e600eccb6b2ab072795e0b28458
“sonia” <soniachaparro21@gmail.com>
2023-01-08 16:28:38 -0300

    add info about

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export const INFO = {
+
+        description:  "I consider myself a persistent person who likes challenges, knowing how to learn new things and put them into practice. I have always been curious and   attracted to technology, but I had never set out to work on something directly related to it. As new information and communication technologies have evolved, I have become aware of their potential and the job opportunities offered by this field. That's why I decided to embark on the adventure of training in software development. The programming language that I like the most is JavaScript, because of its versatility, where I would like to go more into the backend area."
+ 
+}
+
