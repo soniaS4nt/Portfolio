7f2158192ce7da43815c3d4440315e35f0d7f50a
“sonia” <soniachaparro21@gmail.com>
2023-01-09 20:08:12 -0300

    style & props navpage

---
+++ src/components/NavPage/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ContainerPage = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 1rem;
+    gap: 1rem;
+    `
