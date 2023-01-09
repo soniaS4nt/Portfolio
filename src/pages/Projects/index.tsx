f6fccf642f0d3a1ee72e908de3759930f386e1f9
“sonia” <soniachaparro21@gmail.com>
2023-01-09 20:09:12 -0300

    refactor to projects page

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,12 +1,23 @@
 import { Container } from "@chakra-ui/react";
-import { ContainerMadre } from "../../styles/styles";
+import { NavPage } from "../../components";
+import { ContainerMadre, Title } from "../../styles/styles";
 import RepoGithub from "../ReposGitHub";
+import useFetchRepo from "../ReposGitHub/hooks/useFetchRepo";
+import { ContainerGrid } from "../ReposGitHub/styles";
 
 const Projects: React.FC = () => {
+  const { repositories, setPage, page } = useFetchRepo();
+
   return (
     <ContainerMadre>
       <Container centerContent>
-        <RepoGithub />
+        <Title>Projects</Title>
+        {repositories.map((repository) => (
+          <ContainerGrid key={repository.id}>
+            <RepoGithub repository={repository} />
+          </ContainerGrid>
+        ))}
+        <NavPage setPage={setPage} page={page} />
       </Container>
     </ContainerMadre>
   );
--- src/pages/ReposGitHub/index.tsx
+++ src/pages/ReposGitHub/index.tsx
@@ -1,38 +1,26 @@
 import { Button, Card, CardBody, Text } from "@chakra-ui/react";
-import useFetchRepo from "./hooks/useFetchRepo";
-import { Title } from "../../styles/styles";
-import { ContainerGrid } from "./styles";
 import { Link } from "../Footer/styles";
+import { Props } from "./interfaces";
 
-const RepoGithub: React.FC = () => {
-  const { repositories } = useFetchRepo();
-
+const RepoGithub: React.FC<Props> = ({ repository }) => {
   return (
-    <>
-      <Title>Projects</Title>
-      <ContainerGrid>
-        {repositories.map((repository) => (
-          <Card key={repository.id} margin="2">
-            <CardBody margin="2">
-              <Text color="#F50057" fontWeight={"bold"}>
-                {repository.name}
-              </Text>
-              <Text>{repository.description}</Text>
-
-              <Button margin="2">
-                <Link
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={repository.html_url}
-                >
-                  Acceder
-                </Link>
-              </Button>
-            </CardBody>
-          </Card>
-        ))}
-      </ContainerGrid>
-    </>
+    <Card key={repository.id} margin="2">
+      <CardBody margin="2">
+        <Text color="#F50057" fontWeight={"bold"}>
+          {repository.name}
+        </Text>
+        <Text>{repository.description}</Text>
+        <Button margin="2">
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href={repository.html_url}
+          >
+            Acceder
+          </Link>
+        </Button>
+      </CardBody>
+    </Card>
   );
 };
 
