0db38914910b8d44d602a80e76c718640a36b549
“sonia” <soniachaparro21@gmail.com>
2023-01-09 00:04:05 -0300

    Change navbar

--- src/components/Layout/components/MainMenu/index.tsx
+++ src/components/Layout/components/MainMenu/index.tsx
@@ -1,59 +1,47 @@
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 import {
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Show,
-} from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+  NavbarContainer,
+  NavbarExtendedContainer,
+  NavbarInnerContainer,
+  NavbarLink,
+  NavbarLinkContainer,
+  NavbarLinkExtended,
+  OpenLinksButton,
+  RightContainer,
+} from "./styles";
 import ButtonColorMode from "../../../ButtonColorMode";
-import { ItemListStyled, MenuIcon } from "./styles";
+//import { Container } from "@chakra-ui/react";
 
 const MainMenu: React.FC = () => {
+  const [extendNavbar, setExtendNavbar] = useState(false);
+
   return (
-    <>
-      <Show breakpoint="(max-width: 400px)">
-        <Menu closeOnSelect={false} closeOnBlur>
-          <MenuButton
-            as={IconButton}
-            aria-label="Options"
-            icon={<HamburgerIcon />}
-            variant="outline"
-          />
-          <MenuList color={"-moz-initial"}>
-            <MenuItem>
-              <Link to="/">Home</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="/about">About</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="/projects">Projects</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="/contact">Contact</Link>
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </Show>
-
-      <Show breakpoint="(min-width: 401px)">
-        <ItemListStyled>
-          <Link to="/">Home</Link>
-
-          <Link to="/about">About</Link>
-
-          <Link to="/projects">Projects</Link>
-
-          <Link to="/contact">Contact</Link>
-        </ItemListStyled>
-      </Show>
-      <MenuIcon>
-        <ButtonColorMode />
-      </MenuIcon>
-    </>
+    <NavbarContainer extendNavbar={extendNavbar}>
+      <ButtonColorMode />
+      <NavbarInnerContainer>
+        <NavbarLinkContainer>
+          <NavbarLink to="/">Home</NavbarLink>
+          <NavbarLink to="/about">About</NavbarLink>
+          <NavbarLink to="/projects">Projects</NavbarLink>
+          <NavbarLink to="/contact">Contact</NavbarLink>
+          <OpenLinksButton
+            onClick={() => {
+              setExtendNavbar((curr) => !curr);
+            }}
+          >
+            {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+          </OpenLinksButton>
+        </NavbarLinkContainer>
+      </NavbarInnerContainer>
+      {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended to="/">Home</NavbarLinkExtended>
+          <NavbarLinkExtended to="/about">About</NavbarLinkExtended>
+          <NavbarLinkExtended to="/projects">Projects</NavbarLinkExtended>
+          <NavbarLinkExtended to="/contact">Contact</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
+    </NavbarContainer>
   );
 };
 
--- src/components/Layout/components/MainMenu/styles.ts
+++ src/components/Layout/components/MainMenu/styles.ts
@@ -1,42 +1,93 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const ItemListStyled = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 3rem;
-  padding: 2rem 1.5rem;
+type NavbarProps = {
+  extendNavbar: boolean;
+};
+
+export const NavbarContainer = styled.nav`
   width: 100%;
-  a {
-    font-size: 1rem;
-    font-weight: 700;
-    transition: transform .2s;
+  height: ${(props: NavbarProps) => (props.extendNavbar ? "100vh" : "80px")};
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 700px) {
+    height: 80px;
   }
+`;
 
-a:hover {
-    color: orangered;
-    text-decoration: none;
+export const LeftContainer = styled.div`
+  flex: 70%;
+  display: flex;
+  align-items: center;
+  padding-left: 5%;
+`;
+
+export const RightContainer = styled.div`
+  flex: 30%;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 50px;
+`;
+
+export const NavbarInnerContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  @media (min-width: 700px) {
+  align-items: center;
+  justify-content: center;
+  }
+`;
+
+export const NavbarLinkContainer = styled.div`
+  display: flex;
+`;
+
+export const NavbarLink = styled(Link)`
+  font-size: x-large;
+  text-decoration: none;
+  margin: 10px;
+  transition: transform .2s;
+  &:hover {
     transform: scale(1.2);
+    color: orangered;
+  }
+  @media (max-width: 700px) {
+    display: none;
   }
 
+`;
 
-  @media (max-width: 500px) {
-    font-size: 1rem;
-    gap: 1rem;
-    a {
-    font-size: 1rem;
-    font-weight: 300;
+export const NavbarLinkExtended = styled(Link)`
+  font-size: x-large;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+`;
+
+export const Logo = styled.img`
+  margin: 10px;
+  max-width: 180px;
+  height: auto;
+`;
+
+export const OpenLinksButton = styled.button`
+  width: 70px;
+  height: 50px;
+  background: none;
+  border: none;
+  font-size: 45px;
+  cursor: pointer;
+  @media (min-width: 700px) {
+    display: none;
   }
+`;
+
+export const NavbarExtendedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (min-width: 700px) {
+    display: none;
   }
-`
-
-export const MenuIcon = styled.div`
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 60%);
-` 
-
-
-
+`;
