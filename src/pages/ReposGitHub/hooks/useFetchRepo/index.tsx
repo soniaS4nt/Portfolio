b502e971aad903a3c5742731a0c50e4f43ebe301
“sonia” <soniachaparro21@gmail.com>
2023-01-09 20:10:00 -0300

    Add state to navPage

--- src/pages/ReposGitHub/hooks/useFetchRepo/index.tsx
+++ src/pages/ReposGitHub/hooks/useFetchRepo/index.tsx
@@ -4,13 +4,22 @@ import { Repository } from "../../interfaces";
 
 const useFetchRepo = () => {
   const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [page, setPage] = useState(1);
+
+  const Repo = {
+    per_page: 2,
+    owner: "soniaS4nt",
+  };
 
   useEffect(() => {
-    api.get("users/soniaS4nt/repos").then((response) => {
-      setRepositories(response.data);
-    });
-  }, []);
+    const { per_page, owner } = Repo;
+    api
+      .get(`users/${owner}/repos?per_page=${per_page}&page=${page}`)
+      .then((response) => {
+        setRepositories(response.data);
+      });
+  }, [page]);
 
-  return { repositories };
+  return { repositories, setPage, page };
 };
 export default useFetchRepo;
