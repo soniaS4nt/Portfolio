46f52a8b102898ebf9e0039fcd47d5655e2c9fbe
“sonia” <soniachaparro21@gmail.com>
2023-01-10 01:50:10 -0300

    fix style navPage

--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
 export const ContainerNavPagebottom = styled.div`
+
+@media (min-width: 700px) { 
     position: absolute;
     margin-bottom: 5rem ;
     bottom: 0;
     width: 50%;
-    `
+
+}
+  `
