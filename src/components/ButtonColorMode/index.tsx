92e12a59949fd515a3be598928fda26e882f8dd1
“sonia” <soniachaparro21@gmail.com>
2023-01-10 13:38:07 -0300

    fix icon button

--- src/components/ButtonColorMode/index.tsx
+++ src/components/ButtonColorMode/index.tsx
@@ -4,9 +4,11 @@ import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 const ButtonColorMode: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <IconButton onClick={toggleColorMode} aria-label={"Button mode"}>
-      {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-    </IconButton>
+    <IconButton
+      onClick={toggleColorMode}
+      aria-label={"Button mode"}
+      icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+    />
   );
 };
 
