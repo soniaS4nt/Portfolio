3f8f4d1dcd0ee2db777bf92a5f30cceecba829c5
“sonia” <soniachaparro21@gmail.com>
2023-01-10 16:08:17 -0300

    Add position to buttonColor

---
+++ src/components/ButtonColorMode/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerFix = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    @media (min-width: 700px) {
+        margin: 1rem;
+    }
+    `
