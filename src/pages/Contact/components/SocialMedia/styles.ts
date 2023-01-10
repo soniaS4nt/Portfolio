2ec0af0462c5eee33d553a8e3d96dc005a3b1778
“sonia” <soniachaparro21@gmail.com>
2023-01-10 01:16:32 -0300

    Add style to social media

---
+++ src/pages/Contact/components/SocialMedia/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const SocialMediaContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem;
+`;
+
+export const SocialMediaWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+`;
+
+export const SocialMediaLink = styled.a`
+    font-size: 1.5rem;
+    margin: 0 1rem;
+`;
+
+export const SocialMediaIcon = styled.img`
+    width: 3rem;
+    height: 3rem;
+`;
+
+export const SocialMediaText = styled.p`
+    font-size: 1rem;
+    margin:  1rem;
+`;
