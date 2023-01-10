6cd9dd895eff9e2000c3dcba58c006d5ff2f83f0
“sonia” <soniachaparro21@gmail.com>
2023-01-10 01:19:14 -0300

    Add info from other pages

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,39 @@
 export const INFO = {
 
-        description:  "I consider myself a persistent person who likes challenges, knowing how to learn new things and put them into practice. I have always been curious and   attracted to technology, but I had never set out to work on something directly related to it. As new information and communication technologies have evolved, I have become aware of their potential and the job opportunities offered by this field. That's why I decided to embark on the adventure of training in software development. The programming language that I like the most is JavaScript, because of its versatility, where I would like to go more into the backend area."
- 
+        description:  "I consider myself a persistent person who likes challenges, knowing how to learn new things and put them into practice. I have always been curious and   attracted to technology, but I had never set out to work on something directly related to it. As new information and communication technologies have evolved, I have become aware of their potential and the job opportunities offered by this field. That's why I decided to embark on the adventure of training in software development. The programming language that I like the most is JavaScript, because of its versatility, where I would like to go more into the backend area.",
+
+        socialMedia : "Hello! Thank you for visiting my website. If you have any questions or suggestions, please don't hesitate to contact me. You can use the contact form below or send me an email. I'm looking forward to hearing from you!",
+
+        noFound: "Sorry, the page you are looking for does not exist. Please check the URL or go back to the home page. Thank you!",
+
+        projects : 'Here you can see some of the projects I have done. I hope you like them!',
+
+        skills: [ 
+                "JavaScript",
+                "TypeScript",
+                "React",
+                "Redux",
+                "NodeJS",
+                "NestJS",
+                "MongoDB",
+                "MySQL",
+                "Express",
+                "Git",
+                "HTML",
+                "CSS",
+                "SASS",
+                "Bootstrap",
+                "Material UI",
+                "Styled Components",
+                "Jest",
+                "Heroku",
+                "Netlify",
+                "Firebase",
+                "Postman",
+                "Agile Methodologies",
+                "SCRUM",
+                "Trello",
+                "Figma",
+        ],
 }
 
