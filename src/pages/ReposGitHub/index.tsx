75f41d9401fdfe3d21e37e9c6eb1b5f8cf189247
“sonia” <soniachaparro21@gmail.com>
2023-01-10 01:18:08 -0300

    Change button and style

--- src/pages/ReposGitHub/index.tsx
+++ src/pages/ReposGitHub/index.tsx
@@ -1,24 +1,30 @@
-import { Button, Card, CardBody, Text } from "@chakra-ui/react";
+import { Button, Card, CardBody, IconButton, Text } from "@chakra-ui/react";
 import { Link } from "../Footer/styles";
 import { Props } from "./interfaces";
+import github from "../../../public/github.svg";
+import { ContainerRepo } from "./styles";
 
 const RepoGithub: React.FC<Props> = ({ repository }) => {
   return (
     <Card key={repository.id} margin="2">
       <CardBody margin="2">
-        <Text color="#F50057" fontWeight={"bold"}>
+        <Text color="#F50057" fontWeight="bold" fontSize="2xl">
           {repository.name}
         </Text>
         <Text>{repository.description}</Text>
-        <Button margin="2">
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={repository.html_url}
-          >
-            Acceder
-          </Link>
-        </Button>
+        <ContainerRepo>
+          <Text color="#ee5122" fontSize="small" aria-label="language">
+            {repository.language}
+          </Text>
+          <Button margin="2" aria-label="Github">
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href={repository.html_url}
+            />
+            <img src={github} alt="Github" style={{ width: "1.5rem" }} />
+          </Button>
+        </ContainerRepo>
       </CardBody>
     </Card>
   );
