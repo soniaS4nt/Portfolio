3eaa5c7c106395b8fcff26b0eb836b3c5824dbe5
“sonia” <soniachaparro21@gmail.com>
2023-01-10 01:15:30 -0300

    delete coments

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,20 +1,12 @@
 import { Container } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import about from "../../about.json";
 import { Title, ContainerMadre } from "../../styles/styles";
+import { INFO } from "../../utils";
 const About: React.FC = () => {
-  const [language, setLanguage] = useState("En");
-  const [content, setContent] = useState({ description: "" });
-
-  useEffect(() => {
-    language == "En" ? setContent(about.En) : setContent(about.Es);
-  });
-
   return (
     <ContainerMadre>
       <Container centerContent>
         <Title>About</Title>
-        <p>{content.description}</p>
+        <p>{INFO.description}</p>
       </Container>
     </ContainerMadre>
   );
