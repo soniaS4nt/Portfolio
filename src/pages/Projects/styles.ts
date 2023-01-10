64a192b6dd950d5fbbc342bad1c9263cca79d0d8
“sonia” <soniachaparro21@gmail.com>
2023-01-10 01:17:29 -0300

    Add style

---
+++ src/pages/Projects/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ContainerNavPagebottom = styled.div`
+    position: absolute;
+    margin-bottom: 5rem ;
+    bottom: 0;
+    width: 50%;
+    `
