173e0b28225a9d9720402229729d67125da7b585
“sonia” <soniachaparro21@gmail.com>
2023-01-12 23:50:07 -0300

    Add types

---
+++ src/pages/Contact/components/hooks/index.ts
@@ -0,0 +1,41 @@
+import { useDisclosure } from "@chakra-ui/react";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+import { redirect } from "react-router-dom";
+
+
+export const useEmailJs = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = useRef(null);
+  const finalRef = useRef(null);
+  const form = useRef({} as HTMLFormElement);
+  
+
+  const sendEmail =  (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  
+    
+    emailjs
+    .sendForm(
+      import.meta.env.VITE_YOUR_SERVICE_ID,
+      import.meta.env.VITE_APP_YOUR_TEMPLATE_ID,
+      form.current,
+      import.meta.env.VITE_YOUR_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          
+          if(result.status === 200)
+            window.location.href = "/";
+
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+  };
+
+  return { form, sendEmail, isOpen, onOpen, onClose, initialRef, finalRef };
+};
