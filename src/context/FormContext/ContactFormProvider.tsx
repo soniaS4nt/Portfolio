538ab7095bb6cabe9fb57f4353be7c2992077424
“sonia” <soniachaparro21@gmail.com>
2023-01-12 23:48:23 -0300

    Add context to form

---
+++ src/context/FormContext/ContactFormProvider.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren, useState } from "react";
+import { initialState, ContactFormContext } from "./index";
+import { IContactForm } from "../../models/contactForm.model";
+
+export const ContactFormProvider: React.FC<PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [form, setForm] = useState<IContactForm>(initialState.form);
+
+  return (
+    <ContactFormContext.Provider
+      value={{
+        form,
+        setForm,
+      }}
+    >
+      {children}
+    </ContactFormContext.Provider>
+  );
+};
